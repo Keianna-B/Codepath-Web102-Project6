d5e2215265d269b259e200e6df36292140a71115
Keianna <keiannabrown101@gmail.com>
2025-04-07 00:55:09 -0500

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar';
 import Dashboard from './components/Dashboard';
+import Amiibo from './components/Amiibo';
+import {Link} from "react-router-dom";
+import {useRoutes} from "react-router-dom";
+
+
 
 function App() {
   const [list, setList] = useState([]);
   const [filteredResults, setFilteredResults] = useState(list);
   const [search, setSearch] = useState(""  );
-  const [attribute, searchAttribute] = useState('');
+  const [attribute, setAttribute] = useState('');
   const [amiiboSeries, setAmiiboSeries] = useState([]);
-  let mostComonAmiiboSeries = "";
-  let mostCommonAmiiboSeriesCount = 0;
+  const [gameSeries, setGameSeries] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,15 +25,31 @@ function App() {
         setList(data.amiibo)
       } 
     fetchData().catch(console.error)
+
+    const fetchGameSeries = async () => {
+      const response = await fetch(
+        `https://www.amiiboapi.com/api/gameseries/`
+      )
+      const data = await response.json()
+      setGameSeries(data.amiibo)
+    }
+    fetchGameSeries().catch(console.error)
+
+    const fetchAmiiboSeries = async () => {
+      const response = await fetch(
+        `https://www.amiiboapi.com/api/amiiboseries/`
+      )
+      const data = await response.json()
+      setAmiiboSeries(data.amiibo)
+    }
+    fetchAmiiboSeries().catch(console.error)
+
+
   }, [])
 
-  const fetchAmiiboSeries = async () => {
-    const response = await fetch(
-      `https://www.amiiboapi.com/api/amiiboSeries/`
-    )
-    const data = await response.json()
-    setAmiiboSeries(data.amiibo)
-  }
+
+
+
  const getAmiiboSeries = async () => {
   for(let i = 0; i < amiiboSeries.length; i++) {
     if(mostCommonAmiiboSeriesCount !== 0) {
@@ -38,11 +58,17 @@ function App() {
   }
  }
 
-  const searchItem = searchValue => {
-    setSearch(searchValue);
+  const searchItem = (searchValue, attribute) => {
     if (searchValue !== "") {
-      const amiibos = list.filter((item)=>item.character.toLowerCase().includes(searchValue.toLowerCase()));
-      setFilteredResults(amiibos);
+      if(attribute === "name") {
+        setSearch(searchValue);
+        const amiibos = list.filter((item)=>item.name.toLowerCase().includes(searchValue.toLowerCase()));
+        setFilteredResults(amiibos);
+      }
+      else if(attribute === "type") {
+        const amiibos = list.filter((item)=>item.type.toLowerCase().includes(searchValue.toLowerCase()));
+        setFilteredResults(amiibos);
+      }
     } 
     else {
       setFilteredResults(list);
@@ -50,8 +76,7 @@ function App() {
 
   };
 
-  const filterAttribute = (attributeValue, attribute) => {
-    searchAttribute(attributeValue, attribute);
+  const filterType = (attributeValue) => {
     if(attributeValue === "0") {
       attributeValue = "Card";
     }
@@ -61,14 +86,29 @@ function App() {
     else if(attributeValue === "2") {
       attributeValue = "Yarn";
     }
-    if (attributeValue !== "" && attribute !== "") {
-      const amiibos = list.filter((item)=>item.attribute===attributeValue);
+    else {
+      attributeValue = "";
+    }
+    setAttribute(attributeValue);
+    if (attributeValue !== "") {
+      searchItem(attributeValue, "type");
+      /*
+      if(filteredResults.length > 0) {
+
+        searchItem(search, attribute);
+        //const amiibos = filteredResults.filter((item)=>item.type.toLowerCase().includes(attributeValue.toLowerCase()));
+      }
+      else{
+
+        //const amiibos = list.filter((item)=>item.type.toLowerCase().includes(attributeValue.toLowerCase()));
+      }*/
       setFilteredResults(amiibos);
     } 
   }
 
 
   return (
+    <>
       <div className="whole-page">
       <NavBar />
       <div className="sub-dash">
@@ -77,25 +117,34 @@ function App() {
           <h3>Amiibos</h3>
           </div>
           <div className="sub-section">
-          <h1>{}</h1>
-          <h3>Games</h3>
+          <h1>{gameSeries.length}</h1>
+          <h3>Game Series</h3>
           </div>
           <div className="sub-section">
-          <h1></h1>
-          <h3>Most Common Amiibo Series: </h3>
+          <h1>{amiiboSeries.length}</h1>
+          <h3>Amiibo Series: </h3>
         </div>
       </div>
 
-
         <div className="header">
-        <h1>Amiibo Search</h1>
-        <h4>Find an amiibo by its name.</h4>
+          <h1>Amiibo Search</h1>
+          <h4>Find an amiibo by its name.</h4>
         
-        <input
-            type="text"
-            placeholder="Search..."
-            onChange={(inputString) => searchItem(inputString.target.value)}
-          />
+          <input
+              type="text"
+              placeholder="Search Amiibos..."
+              onChange={(inputString) => searchItem(inputString.target.value, "name")}
+            />
+          <form>
+            <label>Type (Card/Figure/Yarn/All):</label>
+            <input
+              type="range"
+              min="0"
+              max="3"
+              onChange={
+                (e) => props.filterType(e.target.value)}
+            />
+          </form>
         </div>
 
         <br></br>
@@ -107,34 +156,49 @@ function App() {
               <th>Game Series</th>
               <th>Amiibo Series</th>
               <th>Type</th>
+              <th>Link</th>
             </tr>
           </thead>
           <tbody>
-          {searchItem.length > 0
-                ? (filteredResults.map((item) => (
-                <MiniCard 
-                  id={item.head + item.tail} 
-                  name={item.name}
-                  gameSeries={item.gameSeries}
-                  amiiboSeries={item.amiiboSeries}
-                  type={item.type}
-                />
+           {filteredResults.length > 0
+                ?(filteredResults.map((item) => (
+                  <tr key={item.head+item.tail}>
+                    <td>{item.name}</td>
+                    <td>{item.gameSeries}</td>
+                    <td>{item.amiiboSeries}</td>
+                    <td>{item.type}</td>
+                    <td>
+                      <Link 
+                      to={`/amiibo/${item.head+item.tail}`}
+                      key={`${item.head+item.tail}`}>
+                        More Info
+                      </Link>
+                    </td>
+                </tr>
               )))
               :(list && list.map((item) => (
-                <MiniCard 
-                  id={item.head + item.tail} 
-                  name={item.name}
-                  gameSeries={item.gameSeries}
-                  amiiboSeries={item.amiiboSeries}
-                  type={item.type}
-                />
-                )))
+                <tr key={item.head+item.tail}>
+                        <td>{item.name}</td>
+                        <td>{item.gameSeries}</td>
+                        <td>{item.amiiboSeries}</td>
+                        <td>{item.type}</td>
+                        <td>
+                        <Link 
+                        to={`/amiibo/${item.head+item.tail}`}
+                        key={`${item.head+item.tail}`}>
+                        
+                        More Info
+                      </Link>
+                        </td>
+                    </tr>
+              )))
               }
           </tbody>
          
         </table>
 
       </div>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Amiibo from './components/Amiibo.jsx'
+//import NotFound from './components/NotFound.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index={true} path="/" element={<App />} />
+        <Route index={false} path="/amiibo/:id" element={<Amiibo />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
